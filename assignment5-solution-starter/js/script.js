c0e282012954089b2f8758778c7ce6a26ab2724f
Mike <mike@tech.et>
2022-06-09 18:15:23 +0300

    second

--- assignment5-solution-starter/js/script.js
+++ assignment5-solution-starter/js/script.js
@@ -95,6 +95,14 @@ $(function () {
     $ajaxUtils.sendGetRequest(
       homeHtmlUrl,
       function (homeHtml) {
+        var chosenCategoryShortName =
+          chooseRandomCategory(categories).short_name;
+        var homeHtmlToInsertIntoMainPage = insertProperty(
+          homeHtml,
+          "randomCategoryShortName",
+          "'" + chosenCategoryShortName + "'"
+        );
+        insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
         // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
         // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
         // variable's name implies it expects.
